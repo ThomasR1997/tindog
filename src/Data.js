480f0594b38abd4252726721a727471c14f5347b
hftjutyir <thomas.kodehode@gmail.com>
2023-02-20 10:44:00 +0100

    initial commit

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+import rexImg from "./App/dog1.jpg";
+import bellaImg from "./App/dog2.jpg";
+import teddyImg from "./App/dog3.jpg";
+
+export const dogs = [
+  {
+    name: "Rex",
+    avatar: rexImg,
+    age: 25,
+    bio: "Art. Literature. Natural wine. Yoga.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Bella",
+    avatar: bellaImg,
+    age: 43,
+    bio: "Yup, that's my owner. U can meet him if you want",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Teddy",
+    avatar: teddyImg,
+    age: 30,
+    bio: "How you doin?",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+];
---
+++ src/pages/landingPage/LandingPage.js
@@ -0,0 +1,7 @@
+export const LandingPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
